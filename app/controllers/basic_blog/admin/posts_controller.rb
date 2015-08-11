80ed29b77bed6f9ba413b90260de041969c6dc30
Chris Desch <cdesch@gmail.com>
2015-08-11 09:47:52 -0400

    Changing Module in Admin Controller
    Signed-off-by: Chris Desch <cdesch@gmail.com>

--- app/controllers/basic_blog/admin/posts_controller.rb
+++ app/controllers/basic_blog/admin/posts_controller.rb
@@ -1,55 +1,56 @@
 require_dependency "basic_blog/admin_controller"
 
 module BasicBlog
-  class Admin::PostsController < AdminController
+  module Admin
+    class PostsController < AdminController
 
-    before_action :set_post, only: [:show, :edit, :update, :destroy]
+      before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-    # GET /posts
-    def index
-      @posts = Post.all
-    end
-
-    # GET /posts/1
-    def show
-    end
-
-    # GET /posts/new
-    def new
-      @post = Post.new
-    end
-
-    # GET /posts/1/edit
-    def edit
-    end
-
-    # POST /posts
-    def create
-      @post = Post.new(post_params)
-
-      if @post.save
-        redirect_to @post, notice: 'Post was successfully created.'
-      else
-        render :new
+      # GET /posts
+      def index
+        @posts = Post.all
       end
-    end
 
-    # PATCH/PUT /posts/1
-    def update
-      if @post.update(post_params)
-        redirect_to @post, notice: 'Post was successfully updated.'
-      else
-        render :edit
+      # GET /posts/1
+      def show
       end
-    end
 
-    # DELETE /posts/1
-    def destroy
-      @post.destroy
-      redirect_to posts_url, notice: 'Post was successfully destroyed.'
-    end
+      # GET /posts/new
+      def new
+        @post = Post.new
+      end
 
-    private
+      # GET /posts/1/edit
+      def edit
+      end
+
+      # POST /posts
+      def create
+        @post = Post.new(post_params)
+
+        if @post.save
+          redirect_to @post, notice: 'Post was successfully created.'
+        else
+          render :new
+        end
+      end
+
+      # PATCH/PUT /posts/1
+      def update
+        if @post.update(post_params)
+          redirect_to @post, notice: 'Post was successfully updated.'
+        else
+          render :edit
+        end
+      end
+
+      # DELETE /posts/1
+      def destroy
+        @post.destroy
+        redirect_to posts_url, notice: 'Post was successfully destroyed.'
+      end
+
+      private
       # Use callbacks to share common setup or constraints between actions.
       def set_post
         @post = Post.find(params[:id])
@@ -59,5 +60,7 @@ module BasicBlog
       def post_params
         params.require(:post).permit(:title, :content,:excerpt,:tag_list, :published_at)
       end
+    end
   end
+
 end
