c5f7e1b717a4810e39de4804147dc75dfe59f707
Chris Desch <cdesch@gmail.com>
2015-08-11 13:05:16 -0400

    Updating asset pipeline
    Signed-off-by: Chris Desch <cdesch@gmail.com>

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,1 @@
-Rails.application.config.assets.precompile += %w( basic_blog/application.js basic_blog/posts.js ckeditor/*  )
+Rails.application.config.assets.precompile += %w( jquery_ujs.js basic_blog/application.js basic_blog/posts.js ckeditor/*  )
